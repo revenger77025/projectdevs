e03c859104e579bf0e5eae4cf4e70950ec7bf90b
revenger77025 <143523189+revenger77025@users.noreply.github.com>
2023-12-21 18:04:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import mobileNav from './src/utils/mobile-nav'
+import mobileNav from './src/utils/mobile-nav';
 import '/styles/header.css';
 import '/styles/main.css';
 import '/styles/mobile-nav.css';
