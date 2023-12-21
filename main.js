3c64aea80a7545374bb80344819fe11890437eae
revenger77025 <143523189+revenger77025@users.noreply.github.com>
2023-12-21 18:02:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import mobileNav from './src/utils/mobile-nav'
-import '/styles/header.css'
-import '/styles/main.css'
-import '/styles/mobile-nav.css'
-import '/styles/payment.css'
-import '/styles/style.css'
-import '/styles/utils.css'
+import '/styles/header.css';
+import '/styles/main.css';
+import '/styles/mobile-nav.css';
+import '/styles/payment.css';
+import '/styles/style.css';
+import '/styles/utils.css';
+import '/styles/fees.css';
 mobileNav();
 
